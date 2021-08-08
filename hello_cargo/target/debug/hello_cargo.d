d6f33bb9f554026404662ac1f918e8882d8a675a
TVilella <vilelladev@outlook.com>
2021-08-08 20:17:43 -0300

    Second step, using cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/tvilella/rust_projects/Learning/hello_cargo/target/debug/hello_cargo: /home/tvilella/rust_projects/Learning/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/tvilella/rust_projects/Learning/hello_cargo/target/release/hello_cargo: /home/tvilella/rust_projects/Learning/hello_cargo/src/main.rs
