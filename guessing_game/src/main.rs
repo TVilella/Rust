7d05b7dd5a14c5c2367b662b622a56dde899ebf8
TVilella <vilelladev@outlook.com>
2021-08-08 21:47:19 -0300

    Segue o fluxo!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,12 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Pense em um número!");
 
-    let secret_number = rand::thread_rng()
-				.gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("O número secreto é: {}", secret_number);
 
     println!("Por favor digite o número que pensou.");
 
@@ -15,6 +16,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Falha ao ler a linha");
 
+    let guess: u32 = guess
+                        .trim()
+                        .parse()
+                        .expect("Por favor, digite um número!");
+
     println!("Voce pensou no número:... {}", guess);
-    println!("O número secreto é: {}", secret_number);
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Muito pequeno!"),
+        Ordering::Greater => println!("Muito grande!"),
+        Ordering::Equal => println!("Aew!! Acertou!"),
+    }
 }
